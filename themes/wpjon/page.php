e781c42b89e7d2521d901bdc309ea62264bbbd68
jontronics-og <jon.wordpress.dev@gmail.com>
2024-11-19 09:45:36 -0500

    Refactored code via template parts

--- themes/wpjon/page.php
+++ themes/wpjon/page.php
@@ -1,32 +1,23 @@
 <?php get_header(); ?>
+
 <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                        <div class="container">
-                            <div class="page-item">
-                                <?php 
-                                    if( have_posts() ):
-                                        while( have_posts() ) : the_post();
-                                        ?>
-                                            <article>
-                                                <header>
-                                                <h1><?php the_title(); ?></h1>
+                    <div class="container">
+                        <div class="page-item">
+                            <?php 
+                                while( have_posts() ) : the_post();
+                                get_template_part( 'parts/content', 'page' );
 
-                                                </header>
-                                           
-                                              
-                                                <?php the_content(); ?>
-                                            </article>
-                                        <?php
-                                        endwhile;
-                                    else: ?>
-                                        <p>Nothing yet to be displayed!</p>
-                                <?php endif; ?>                                
-                            </div>
-                            <?php get_sidebar('page'); ?>
+                                if( comments_open() || get_comments_number() ){
+                                    comments_template();
+                                }
+                                endwhile;
+                            ?>                                
                         </div>
-       
+                    </div>
                 </main>
             </div>
         </div>
--- themes/wpjon/single.php
+++ themes/wpjon/single.php
@@ -7,22 +7,23 @@
             <?php 
             while( have_posts() ):
                 the_post();
+                get_template_part( 'parts/content', 'single' );
                 ?>
-                <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
-                    
-                    <header>
-                        <h1><?php the_title(); ?></h1>
-                        <div class="meta-info">
-                            <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                            <p>Categories: <?php the_category( ' ' ); ?></p>
-                            <p>Tags: <?php the_tags( '', ', ' ); ?></p>     
-                        </div>
-                    </header>
-                    <div class="content">
-                        <?php the_content(); ?>
+                <div class="wpdevs-pagination">
+                    <div class="pages next">
+                        <?php next_post_link( '&laquo; %link' ); ?>
                     </div>
-                </article>
+                    <div class="pages previous">
+                        <?php previous_post_link( '%link &raquo;' ); ?>  
+                    </div>
+                </div>
+                
                 <?php
+
+                if( comments_open() || get_comments_number() ){
+                    comments_template();
+                }
+
             endwhile;
             ?>
 
