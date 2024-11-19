0ef7fa2ae2de7f41e8964704a6aef28a9f524312
jontronics-og <jon.wordpress.dev@gmail.com>
2024-11-19 15:58:28 -0500

    Hero section on home page editable in wp-admin customizer
    Main Title,
    Sub Title,
    Button Text,
    Link to button,
    Background Image

--- themes/wpjon/inc/customizer.php
+++ themes/wpjon/inc/customizer.php
@@ -1,7 +1,7 @@
 <?php
 
 function wpjon_customizer( $wp_customize ){
-    // adding the ability to change copyright info in footer.
+    // adding the ability to change copyright info in footer via the theme customizer.
     $wp_customize->add_section(
         'sec_copyright',
         array(
@@ -28,5 +28,130 @@ function wpjon_customizer( $wp_customize ){
                     'type' => 'text'
                 )
             ); 
+ // 2 Hero
+ $wp_customize->add_section(
+    'sec_hero',
+    array(
+        'title' => 'Hero Section'
+    )
+);
+
+        // Title
+        $wp_customize->add_setting(
+            'set_hero_title',
+            array(
+                'type' => 'theme_mod',
+                'default' => 'Please, add some title',
+                'sanitize_callback' => 'sanitize_text_field'
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_title',
+            array(
+                'label' => 'Hero Title',
+                'description' => 'Please, type your here title here',
+                'section' => 'sec_hero',
+                'type' => 'text'
+            )
+        );  
+        
+        // Subtitle
+        $wp_customize->add_setting(
+            'set_hero_subtitle',
+            array(
+                'type' => 'theme_mod',
+                'default' => 'Please, add some subtitle',
+                'sanitize_callback' => 'sanitize_textarea_field'
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_subtitle',
+            array(
+                'label' => 'Hero Subtitle',
+                'description' => 'Please, type your subtitle here',
+                'section' => 'sec_hero',
+                'type' => 'textarea'
+            )
+        );
+
+        // Button Text
+        $wp_customize->add_setting(
+            'set_hero_button_text',
+            array(
+                'type' => 'theme_mod',
+                'default' => 'Learn More',
+                'sanitize_callback' => 'sanitize_text_field'
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_hero_button_text',
+            array(
+                'label' => 'Hero button text',
+                'description' => 'Please, type your hero button text here',
+                'section' => 'sec_hero',
+                'type' => 'text'
+            )
+        );
+
+        // Button link
+      $wp_customize->add_setting(
+          'set_hero_button_link',
+          array(
+              'type' => 'theme_mod',
+              'default' => '#',
+              'sanitize_callback' => 'esc_url_raw'
+          )
+      );
+
+      $wp_customize->add_control(
+          'set_hero_button_link',
+          array(
+              'label' => 'Hero button link',
+              'description' => 'Please, type your hero button link here',
+              'section' => 'sec_hero',
+              'type' => 'url'
+          )
+      ); 
+      
+      // Hero Height
+      $wp_customize->add_setting(
+          'set_hero_height',
+          array(
+              'type' => 'theme_mod',
+              'default' => 800,
+              'sanitize_callback' => 'absint'
+          )
+      );
+
+      $wp_customize->add_control(
+          'set_hero_height',
+          array(
+              'label' => 'Hero height',
+              'description' => 'Please, type your hero height',
+              'section' => 'sec_hero',
+              'type' => 'number'
+          )
+      );
+
+      // Hero Background
+    $wp_customize->add_setting(
+        'set_hero_background',
+        array(
+            'type' => 'theme_mod',
+            'sanitize_callback' => 'absint'
+        )
+    );
+
+    $wp_customize->add_control( new WP_Customize_Media_Control( $wp_customize,
+        'set_hero_background',
+        array(
+            'label' => 'Hero Image',
+            'section'   => 'sec_hero',
+            'mime_type' => 'image'
+        )));
+
 }
 add_action( 'customize_register', 'wpjon_customizer' );
